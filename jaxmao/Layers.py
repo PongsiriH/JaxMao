d1bf82763eb67a47ffd2d84ff99035e83a3c33c5
rengarism <maowhy.1@gmail.com>
2023-09-08 15:48:56 +0700

    class Layer as default __init__ and init_params
    default def init_params by looping through keys of self.shapes and self.initializers to define self.params.

--- jaxmao/Layers.py
+++ jaxmao/Layers.py
@@ -5,9 +5,13 @@ from jax import vmap, lax
 from jax import random
 
 class Layer:
-    def __init__(self):
-        self.params = None
-    
+    def __init__(self, dtype=jnp.float32):
+        self.dtype = dtype
+
+        self.params = dict()
+        self.shapes = dict()
+        self.initializers = dict()
+        
     def forward(self, params, x):
         return vmap(self._forward, in_axes=(None, 0))(params, x)
     
@@ -15,7 +19,10 @@ class Layer:
         return self.forward(self.params, x)
     
     def init_params(self, key):
-        pass
+        for layer in self.shapes.keys():
+            key, subkey = random.split(key)
+            self.params[layer] = self.initializers[layer](subkey, self.shapes[layer], dtype=self.dtype)
+            
     
 class FC(Layer):
     def __init__(self, 
@@ -26,31 +33,34 @@ class FC(Layer):
                  use_bias=True,
                  dtype=jnp.float32
         ):
+        super().__init__()
         self.in_channels = in_channels
         self.out_channels = out_channels
-        self.weights_initializer = weights_initializer
-        self.bias_initializer = bias_initializer
         self.use_bias = use_bias
-        self.dtype=dtype
+        self.dtype = dtype
         
-        self.shape = dict()
-        self.shape['weights'] = (in_channels, out_channels)
+        self.shapes = dict()
+        self.shapes['weights'] = (in_channels, out_channels)
         if self.use_bias:
-            self.shape['bias'] = (out_channels, )
-        self.params = None
+            self.shapes['biases'] = (out_channels, )
+            
+        self.initializers = {
+            'weights' : weights_initializer,
+            'biases' : bias_initializer
+        }
    
-    def init_params(self, key):
-        self.params = dict()
+    # def init_params(self, key):
+    #     self.params = dict()
         
-        key, w_key, b_key = random.split(key, 3)
-        self.params['weights'] = self.weights_initializer(w_key, self.shape['weights'], self.dtype)
-        if self.use_bias:
-            self.params['bias'] = self.bias_initializer(b_key, self.shape['bias'], self.dtype)
+    #     key, w_key, b_key = random.split(key, 3)
+    #     self.params['weights'] = self.initializers['weights'](w_key, self.shapes['weights'], self.dtype)
+    #     if self.use_bias:
+    #         self.params['biases'] = self.initializers['biases'](b_key, self.shapes['biases'], self.dtype)
             
     def _forward(self, params, x):
         x = x.astype(self.dtype)
         return jnp.add(
-            lax.dot(x, params['weights']), params['bias']
+            lax.dot(x, params['weights']), params['biases']
         )
         
 class Conv2D(Layer):
@@ -60,51 +70,90 @@ class Conv2D(Layer):
                     kernel_size, 
                     strides=1, 
                     use_bias=True, 
-                    padding='valid',
+                    padding='SAME',
                     weights_initializer=HeNormal(),
                     bias_initializer=HeNormal(),
                     dtype=jnp.float32
             ):
+            super().__init__()
             self.in_channels = in_channels
             self.out_channels = out_channels
             self.kernel_size = kernel_size
             self.strides = strides
             self.use_bias = use_bias
             self.padding = padding
-            self.weights_initializer = weights_initializer 
-            self.bias_initializer = bias_initializer
             self.dtype = dtype
             
-            self.shape = dict()
+            self.shapes = dict()
             if isinstance(kernel_size, int):
-                self.shape['weights'] = (out_channels, in_channels, kernel_size, kernel_size)
+                self.shapes['weights'] = (out_channels, in_channels, kernel_size, kernel_size)
             elif isinstance(kernel_size, tuple):
-                self.shape['weights'] = (out_channels, in_channels, kernel_size[0], kernel_size[1])
+                self.shapes['weights'] = (out_channels, in_channels, kernel_size[0], kernel_size[1])
             if use_bias:
-                self.shape['bias'] = (out_channels, )
-            self.params = None
-
+                self.shapes['biases'] = (out_channels, )
+                
+            self.initializers = {
+                'weights' : weights_initializer,
+                'biases' : bias_initializer
+            }
             
-        def init_params(self, key):
-            self.params = dict()
-            key, w_key, b_key = random.split(key, 3)
-            self.params['weights'] = self.weights_initializer(w_key, self.shape['weights'], self.dtype)
-            self.params['bias'] = self.bias_initializer(b_key, self.shape['bias'], self.dtype)
+        # def init_params(self, key):
+        #     self.params = dict()
+        #     key, w_key, b_key = random.split(key, 3)
+        #     self.params['weights'] = self.initializers['weights'](w_key, self.shapes['weights'], self.dtype)
+        #     self.params['biases'] = self.initializers['biases'](b_key, self.shapes['biases'], self.dtype)
         
         def forward(self, params, x):
             # ('NCHW', 'OIHW', 'NCHW')
             x = lax.conv_general_dilated(x, params['weights'], 
                                             window_strides=(self.strides, self.strides),
-                                            padding="SAME") 
+                                            padding=self.padding) 
             if self.use_bias:
-                x = lax.add(x, params['bias'][None, :, None, None])
+                x = lax.add(x, params['biases'][None, :, None, None])
             return x
 
 class Flatten(Layer):
     def _forward(self, params, x):
         return x.ravel()
 
-if __name__ == '__main__':
-    fc = FC(5, 5)
-    print(fc)
-    # print(for )
+class SimpleRNN(Layer):
+    def __init__(
+            self, 
+            input_channel, 
+            output_channel,
+            number_layer,
+            use_bias = True,
+            dtype=jnp.float32
+        ):
+        super().__init__()
+        self.input_channel  = input_channel
+        self.output_channel = output_channel
+        self.number_layer   = number_layer
+        self.use_bias       = use_bias
+        self.dtype = dtype
+        
+        self.shape = {
+            'weights_x' : (input_channel, output_channel),
+            'weights_h' : (output_channel, output_channel),
+        }
+        if use_bias:
+            self.shape['biases_h'] = (output_channel, 1)
+        self.params = None
+        
+    def init_params(self, key):
+        Wx_key, Wh_key, bh_key, key = random.split(key, 4)
+        self.params = {
+            'weights_x' : random.normal(Wx_key, self.shape['weights_x']),
+            'weights_h' : random.normal(Wh_key, self.shape['weights_h'])
+        }
+        if self.use_bias:
+            self.params['biases_h'] = random.normal(bh_key, self.shape['biases_h'])
+
+    def _forward(self, x):
+        h  = jnp.zeros(self.output_channel, dtype=self.dtype)
+
+        h = self.params['weights_h'] * h + self.params['weights_x'] * x
+        if self.use_bias:
+            h = h + self.params['biases_h']
+        h = lax.tanh(h)
+        
